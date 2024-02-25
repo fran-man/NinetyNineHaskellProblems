7ef3da2ec137f57404032ee5eedc39a29d31fa04
fran-man <fran.man@hotmail.co.uk>
2024-02-25 20:32:46 +0000

    Problem 9 complete with comments

---
+++ Lists/Problem9-PackConsecutive.hs
@@ -0,0 +1,23 @@
+packConsecutive :: Eq a => [a] -> [[a]]
+packConsecutive [] = []
+packConsecutive [x] = [[x]]
+packConsecutive l = packConsecutiveLists $ mapElementsToLists l
+
+-- Doesn't really do anything, just makes usage more readable
+mapElementsToLists :: [a] -> [[a]]
+mapElementsToLists = map (: [])
+
+-- || Pack Consecutive Lists ||
+-- If two consecutive lists contain the same elements,
+-- we "squash" them together into one list
+packConsecutiveLists :: Eq a => [[a]] -> [[a]]
+packConsecutiveLists [] = []
+packConsecutiveLists (x:xs)
+-- If xs is null (empty), then we are done and x contains all relevant elements
+    | null xs = [x]
+-- If the elements in x are the same as the elements in the first list of xs,
+-- then x and head xs need to be combined together and consd onto the rest of xs
+    | head x == head (head xs) = packConsecutiveLists ((x ++ head xs) : tail xs)
+-- otherwise x is "complete" and the next array in xs has a different element
+-- so we recurse
+    | otherwise = x : packConsecutiveLists xs
